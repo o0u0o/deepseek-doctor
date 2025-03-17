3f9b542e7376a3eaab5c8ecc97d1ef6e648b8c10
yuechuangui <yuechuangui@gmail.com>
2025-03-17 20:08:52 +0800

    发送追加消息给SSE客户端 ADD时间流式输出<

--- src/main/java/com/o0u0o/controller/SSEController.java
+++ src/main/java/com/o0u0o/controller/SSEController.java
@@ -57,4 +57,21 @@ public class SSEController {
         return "OK";
     }
 
+    /**
+     * <h2>发送追加消息给SSE客户端 ADD时间流式输出</h2>
+     * @param userId 用户ID
+     * @param message 消息内容
+     * @return Object
+     * @throws InterruptedException 线程中断异常
+     */
+    @GetMapping( "sendMessageAdd")
+    public Object sendMessageAdd(@RequestParam String userId, @RequestParam String message) throws InterruptedException {
+        //测试代码，循环输出内容
+        for (int i = 0; i < 10; i++) {
+            Thread.sleep(200);
+            SSEServer.sendMessage(userId, message + "-" + i, SSEMessageType.ADD);
+        }
+        return "OK";
+    }
+
 }
